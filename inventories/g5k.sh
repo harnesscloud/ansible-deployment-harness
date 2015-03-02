2955025f535376fce2621a3f7112887e8c543974
Mark Stillwell <mark@stillwell.me>
2015-03-02 14:30:18 +0000

    testing..

--- inventories/g5k.sh
+++ inventories/g5k.sh
@@ -6,12 +6,12 @@ if [ $1 = "--host" ] || [ -z "$OAR_NODE_FILE" ]; then
 fi
 
 nodes=($(sort -uV $OAR_NODE_FILE))
+#    "compute"    : [ $(printf '"%s", ' ${nodes[@]:1})]
 
 cat <<EOF
 {
     "controller" : [ "${nodes[0]}" ],
-    "network"    : [ "${nodes[0]}" ],
-    "compute"    : [ $(printf '"%s", ' ${nodes[@]:1})]
+    "network"    : [ "${nodes[0]}" ]
 }
 EOF
 
