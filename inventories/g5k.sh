9e0599dc2bcfa6cf3527c61954ce9b4f693635ac
Mark Stillwell <mark@stillwell.me>
2015-03-02 14:38:10 +0000

    should work...

--- inventories/g5k.sh
+++ inventories/g5k.sh
@@ -6,12 +6,12 @@ if [ $1 = "--host" ] || [ -z "$OAR_NODE_FILE" ]; then
 fi
 
 nodes=($(sort -uV $OAR_NODE_FILE))
-#    "compute"    : [ $(printf '"%s", ' ${nodes[@]:1})]
 
 cat <<EOF
 {
     "controller" : [ "${nodes[0]}" ],
     "network"    : [ "${nodes[0]}" ]
+    "compute"    : [ "$(echo ${nodes[@]:1} | perl -lane 'print join "\", \"", @F')" ]
 }
 EOF
 
