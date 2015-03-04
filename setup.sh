ff150c455101c44515583a5bc519b031df9a798b
Mark Stillwell <mark@stillwell.me>
2015-03-04 15:00:22 +0000

    fix first bug in setup script...

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-kadeploy -f $OAR_NODE_FILE -e ubuntu-x64-1404 -k
+#!/bin/bash
+kadeploy3 -f $OAR_NODE_FILE -e ubuntu-x64-1404 -k
 ansible-playbook -i inventories/g5k.sh provisioning/prep.yml
 ansible-playbook -i inventories/g5k.sh provisioning/deploy.yml
