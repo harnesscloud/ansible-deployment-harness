ac6cb29210bc32d2c120d43af4b2ee4d9ecb0882
Mark Stillwell <mark@stillwell.me>
2015-03-04 23:08:50 +0000

    no longer need setup.sh...

--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-kadeploy3 -f $OAR_NODE_FILE -e ubuntu-x64-1404 -k
-ansible-playbook -i inventories/g5k.sh provisioning/prep.yml
-ansible-playbook -i inventories/g5k.sh provisioning/deploy.yml
